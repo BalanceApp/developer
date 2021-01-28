8213b5c9706d794b6e2d5eb610026147cccb9e43
kousukeuo <kousuke.uo.2019b@mlab.info>
2021-01-28 15:39:21 +0900

    自動変更

--- BalanceUP/vendor/composer/autoload_real.php
+++ BalanceUP/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitf831994203c010d9e9da26a03da22b30
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf831994203c010d9e9da26a03da22b30', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf831994203c010d9e9da26a03da22b30', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitf831994203c010d9e9da26a03da22b30::getInitializer($loader));
         } else {
