e88893848dfc532b092d8f58ffc8eb89703f623d
kousukeuo <kousuke.uo.2019b@mlab.info>
2021-04-15 10:16:34 +0000

    スタッフ画面のプレイヤー一覧を更新

--- BalanceUP/resources/views/playerlist.blade.php
+++ BalanceUP/resources/views/playerlist.blade.php
@@ -4,26 +4,23 @@
 <script src="{{asset('js/jquery.min.js')}}"></script>
 <script src="{{asset('js/playerList.js')}}"></script>
 
-<!--begin::Container-->
 <div class="container">
-   <!--begin::Dashboard-->
-   <!--begin::Row-->
    <div class="row">
       <div class="col-xl-12">
-         <!--begin::Tiles Widget 1-->
          <div class="card card-custom gutter-b card-stretch" id="kt_page_stretched_card">
-            <!--begin::Body-->
-            <div class="card-body d-flex flex-column px-0" style="text-align: center; min-height:400px">
-               <div class="row col-md-12">
-                  <a href="/outputCSV" class="headBtn col-sm-2"
-                     style="border-style: solid solid solid solid; margin-left: 3%">CSV出力へ</a>
+            <div class="card-body d-flex flex-column px-0">
+               <div class="row">
+                  <div class="col-2 ml-6">
+                     <a href="/outputCSV" class="headBtn" style="border-style: solid solid solid solid;">CSV出力へ</a>
+                  </div>
                </div>
-               <div class="card-scroll col-md-10" id="staffinfo" style="font-size:18px; margin-top: 50px">
-                  <div class="container">
+               <div class="row mt-10">
+                  <div class="col-11 mx-auto">
                      <table id="table-id" class="table table-bordered gridview">
                         <thead>
                            <tr>
                               <th>ユーザーID</th>
+                              <th>氏名</th>
                               <th>食事チェック結果</th>
                               <th>からだの変化</th>
                            </tr>
@@ -33,6 +30,7 @@
                            @foreach($playerlist as $value)
                            <tr>
                               <td> {{ $value->userid }} </td>
+                              <td> {{ $value->name }} </td>
                               <td><a href="{{url('/result/'.$value->userid)}}">食事チェック結果</a></td>
                               <td><a href="{{url('/view-body-graph/'.$value->userid)}}">からだの変化</a></td>
                            </tr>
@@ -40,23 +38,15 @@
                            @endisset
                         </tbody>
                      </table>
-                     <div style="text-align: right;padding-right: 100px;margin-top: 30px;">
-                        <div>
-                           <a href="{{url('/staff-page')}}"><span class="btn btn-primary btn-lg"
-                                 style="border-radius: 5px; min-width: 100px">戻る</span></a>
-                        </div>
-                     </div>
+                  </div>
+               </div>
+               <div class="row mt-10 mr-13">
+                  <div class="col-12">
+                     <a href="{{url('/staff-page')}}" class="float-right"><span class="btn btn-primary btn-lg">戻る</span></a>
                   </div>
                </div>
             </div>
-            <!--end::Body-->
          </div>
-         <!--end::Tiles Widget 1-->
       </div>
-   </div>
-   <!--end::Row-->
-   <!--end::Dashboard-->
-</div>
-<!--end::Container-->
 
-@stop
+      @stop
